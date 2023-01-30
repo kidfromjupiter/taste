b907ecfcace2daec94cb8faf304e18de0a30d3c5
kidfromjupiter <limahaliyana@gmail.com>
2023-01-30 21:38:37 +0530

    order tracking status indicators complete

--- taste-frontend/src/app/trackorders/page.tsx
+++ taste-frontend/src/app/trackorders/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import OrderStatusSVG from "@/components/OrderStatusSVG";
 import React, { useRef, useState, useLayoutEffect } from "react";
+import { TfiPackage } from "react-icons/tfi";
+import { TbChartArcs, TbGridDots, TbTruckDelivery } from "react-icons/tb";
 
 type Props = {
 	stops: number;
@@ -20,44 +22,90 @@ const TrackOrder = ({ stops = 3 }: Props) => {
 				</div>
 			</div>
 			<div className="flex flex-col font-semibold mt-4">
-				<div className="text-gray-600 text-2xl mb-5">
+				<div className="text-gray-600 text-2xl mb-10">
 					<h3>ETA: Thu, 20th September</h3>
 				</div>
 				<div className="flex h-full overflow-visible">
-					<OrderStatusSVG
-						stops={[
-							{
-								colored: true,
-								primaryColor: "#22c55e",
-								pulseColor: "#86efac",
-								secondaryColor: "#9ca3af",
-							},
+					<div className="flex flex-[1]">
+						<OrderStatusSVG
+							stops={[
+								{
+									colored: true,
+									primaryColor: "#22c55e",
+									pulseColor: "#86efac",
+									secondaryColor: "#9ca3af",
+								},
 
-							{
-								colored: true,
-								primaryColor: "#22c55e",
-								pulseColor: "#86efac",
-								secondaryColor: "#9ca3af",
-							},
-							{
-								colored: false,
-								primaryColor: "#22c55e",
-								pulseColor: "#86efac",
-								secondaryColor: "#9ca3af",
-							},
-							{
-								colored: false,
-								primaryColor: "#22c55e",
-								pulseColor: "#86efac",
-								secondaryColor: "#9ca3af",
-							},
-						]}
-					/>
-					<div>
-						<div className="h-32 pl-5 pr-24">Packaging complete</div>
-						<div className="h-32 pl-5 pr-24">Packaging complete</div>
-						<div className="h-32 pl-5 pr-24">Packaging complete</div>
-						<div className="h-32 pl-5 pr-24">Packaging complete</div>
+								{
+									colored: true,
+									primaryColor: "#22c55e",
+									pulseColor: "#86efac",
+									secondaryColor: "#9ca3af",
+								},
+								{
+									colored: false,
+									primaryColor: "#22c55e",
+									pulseColor: "#86efac",
+									secondaryColor: "#9ca3af",
+								},
+								{
+									colored: false,
+									primaryColor: "#22c55e",
+									pulseColor: "#86efac",
+									secondaryColor: "#9ca3af",
+								},
+							]}
+						/>
+					</div>
+					<div className="flex flex-[4] flex-col">
+						<div className="flex flex-row items-center mb-10">
+							<div className="m-4 mr-6 text-4xl">
+								<TbChartArcs />
+							</div>
+							<div>
+								<div className="font-semibold text-lg">Processing Complete</div>
+								<div className="text-gray-400 font-regular text-md">
+									10:57am, 18th Sep
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-row items-center mb-10">
+							<div className="m-4 mr-6 text-4xl">
+								<TfiPackage />
+							</div>
+							<div>
+								<div className="font-semibold text-lg">Packaging Complete</div>
+								<div className="text-gray-400 font-regular text-md">
+									10:57am, 18th Sep
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-row items-center mb-10">
+							<div className="m-4 mr-6 text-4xl">
+								<TbGridDots />
+							</div>
+							<div>
+								<div className="font-semibold text-lg">
+									Sorted and stored for delivery
+								</div>
+								<div className="text-gray-400 font-regular text-md">
+									10:57am, 18th Sep
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-row items-center mb-10">
+							<div className="m-4 mr-6 text-4xl">
+								<TbTruckDelivery />
+							</div>
+							<div>
+								<div className="font-semibold text-lg">
+									Dispatched for delivery
+								</div>
+								<div className="text-gray-400 font-regular text-md">
+									10:57am, 18th Sep
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
