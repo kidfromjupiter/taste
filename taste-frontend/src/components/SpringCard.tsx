63798c98dccea6c9df451c2a72775cb211340624
kidfromjupiter <limahaliyana@gmail.com>
2023-01-31 08:45:05 +0530

    added overlay to sprincardmodal

--- taste-frontend/src/components/SpringCard.tsx
+++ taste-frontend/src/components/SpringCard.tsx
@@ -11,45 +11,55 @@ type Props = {
 const SpringCard = ({ show = false, hideCallback, title }: Props) => {
 	const [springStyles, springApi] = useSpring(() => ({
 		transform: "translateY(100%)",
-
-		config: {
-			tension: 1000,
-			mass: 1,
-		},
+	}));
+	const [overlayStyles, overlayApi] = useSpring(() => ({
+		opacity: 0,
 	}));
 
 	if (show) {
 		springApi.start({
 			transform: "translateY(0%)",
 		});
+		overlayApi.start({
+			opacity: 1,
+		});
+
 		return (
-			<animated.div
-				style={springStyles}
-				className=" shadow-top bg-gray-100 absolute w-full top-1/3 h-2/3 "
-			>
-				<div className="flex items-center justify-center relative p-3">
-					<div
-						onClick={() => {
-							springApi.start({
-								transform: "translateY(100%)",
-								onRest: () => {
-									hideCallback();
-								},
-							});
-						}}
-					>
-						<h3 className="font-semibold text-xl">{title}</h3>
-						<BsChevronBarDown className=" absolute top-3 right-3 text-4xl text-gray-400" />
-					</div>
-				</div>
-				<div className="h-full w-full overflow-auto">
-					{Array.from({ length: 10 }).map((_, i) => (
-						<div key={i} className="h-24 px-3">
-							Hey
+			<>
+				<animated.div
+					style={springStyles}
+					className=" shadow-top bg-gray-100 absolute w-full top-1/3 h-2/3 overflow-hidden  z-50 pt-5"
+				>
+					<div className="flex items-center justify-center relative p-3">
+						<div>
+							<h3 className="font-semibold text-xl">{title}</h3>
+							{/* <BsChevronBarDown className=" absolute top-3 right-3 text-4xl text-gray-400" /> */}
 						</div>
-					))}
-				</div>
-			</animated.div>
+					</div>
+					<div className="h-full w-full overflow-auto">
+						{Array.from({ length: 10 }).map((_, i) => (
+							<div key={i} className="h-24 px-3">
+								Hey
+							</div>
+						))}
+					</div>
+				</animated.div>
+				<animated.div
+					className="top-0 right-0 h-screen w-screen bg-black bg-opacity-60 absolute z-10"
+					style={overlayStyles}
+					onClick={() => {
+						springApi.start({
+							transform: "translateY(100%)",
+							onRest: () => {
+								hideCallback();
+							},
+						});
+						overlayApi.start({
+							opacity: 0,
+						});
+					}}
+				></animated.div>
+			</>
 		);
 	}
 	return null;
